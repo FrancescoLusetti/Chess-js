cfbf27ae68e8ab7d568831fa9479c82d90bdb721
Luso13FEEDDA <0002luso.ciccio@gmail.com>
2018-01-07 14:42:09 +0100

    playGround done in js

--- Chess.js
+++ Chess.js
@@ -1,11 +1,15 @@
 var Chess = function(T,C){
   this.type = T;
   this.color = C;
+  //TYPE ki=king,q=queen,r=rook,b=bishop,kn=knight,p=pawn
+  //COLOR w=white,b=black
+  this.getType = function() {return this.type;}
+  this.getColor = function() {return this.color;}
+
 }
 
 function Game(){
   //tabella 8*8
-  this.gameTable= new array;
 
   this.tableButtonPLay = '<button style="{z_z_z}" onclick="clickButton({y_y_y})" ><img src="xy"></button>';
   this.HTMLelement = {
@@ -19,8 +23,37 @@ function Game(){
 
   };
 
-  //this.init
+  this.getDim = function () {
+    return 8;
+  }
 
+  this.init = function () {
+    this.playGround = new Array(8);
+    for(let i = 0; i < this.getDim(), i++) this.playGround[i] = new Array (8);
+    this.iniRow(this.playGround[0], false, 'b');
+    this.iniRow(this.playGround[1], true 'b');
+    this.iniRow(this.playGround[6], true, 'w');
+    this.iniRow(this.playGround[7], false, 'w');
+    for(let i = 2; i < 6; i++) this.playGround[i].fill(NULL);
+  }
+
+  this.iniRow = function (row, T, C) {
+    //T: true=row of pawn, false= row of every else;
+
+    if(T){
+      for(let i = 0 ; i < this.getDim() ; i++) row[i]= new Chess('p', C);
+    }
+    else{
+      row[0] = new Chess(r,C);
+      row[1] = new Chess(kn, C);
+      row[2] = new Chess(b,C);
+      row[3] = new Chess(q,C);
+      row[4] = new Chess(ki,C);
+      row[5] = new Chess(b,C);
+      row[6] = new Chess(kn,C);
+      row[7] = new Chess(r,C);
+    }
+  }
 
 
 
@@ -28,10 +61,15 @@ function Game(){
 }
 
 function startGame(){
+
+
 }
 
 
 
 a=0;
+
+turn=0;
+phase=0;
 
 window.onload = startGame(false);
